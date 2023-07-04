811ef87446d312adb4cca3a2cd3d46960e84a3ca
Cuong Nguyen <cuongnt258@gmail.com>
2023-07-04 20:26:32 +0700

    fix nav bar scroll bar

--- src/newDashboard/page/admin/Candidates.js
+++ src/newDashboard/page/admin/Candidates.js
@@ -86,7 +86,7 @@ const useStyles = makeStyles((theme) => ({
     height: '100vh',
     display: 'flex',
     flexDirection: 'column',
-    overflowY: 'scroll',
+    overflowY: 'auto',
     scrollbarWidth: 'thin',
     scrollbarColor: 'red',
   },
